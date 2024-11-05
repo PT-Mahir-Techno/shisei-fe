3dfcc62340ae142a0e812cf75bd4979fd88a56f2
fkr <bangfkr002@gmail.com>
2024-11-05 14:56:02 +0700

    s

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  distDir: 'dist',
+  // output: 'export',
+  // distDir: 'dist',
   images: {
     domains: ['thumbs.dreamstime.com', 'shisei-be.mahirtechno.my.id'],
   }
