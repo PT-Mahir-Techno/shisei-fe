dcae3cbc93c3e46c1bda409e26fc437ec1b3dcaa
fkr <bangfkr002@gmail.com>
2024-11-05 14:13:40 +0700

    s

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: 'export',
+  output: 'export',
   // distDir: './dist',
   images: {
     domains: ['thumbs.dreamstime.com', 'shisei-be.mahirtechno.my.id'],
